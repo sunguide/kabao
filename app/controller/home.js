3c0d4c946cafd3a3afbe268cf7731edb8687ad65
sunguide@qq.com <sunguide1989>
2018-04-20 18:37:03 +0800

    cralwer

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,10 +1,25 @@
 'use strict';
-
+const cheerio = require("cheerio");
+const request = require("superagent");
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'hi, egg';
+
+    let res = await request.get("https://kaku.51credit.com/tag/310100index.html");
+    console.log(res)
+    let $ = cheerio.load(res.text);
+    let banks = [];
+    $('#bank_all a').each(function (i,item) {
+      let bank = {
+        "id":$(item).attr("id"),
+        "small_img":$(item).find("img").attr("src"),
+        "name":$(item).find("em").text()
+      };
+      banks.push(bank);
+    });
+    console.log(banks.length)
+    this.ctx.body = banks;
   }
 }
 
