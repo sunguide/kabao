a1b01bb1157cf8a9e6c8e431fb4f20c337f8a623
sunguide <sunguide@qq.com>
2018-04-20 16:08:21 +0800

    init empty project

---
+++ config/config.default.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1524210999853_2952';
+
+  // add your config here
+  config.middleware = [];
+
+  return config;
+};
